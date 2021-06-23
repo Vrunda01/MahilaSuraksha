a587c469f8efa8a59551feb89d534965dffd5e38
Vrunda01 <vsarvaiya088@rku.ac.in>
2021-06-23 20:15:42 +0530

    Change in the layout of Call Activity activity_call.xml instead od Button inserted Image Button for call icon.

--- app/src/main/java/com/rku/mahilasuraksha/CallActivity.java
+++ app/src/main/java/com/rku/mahilasuraksha/CallActivity.java
@@ -6,26 +6,36 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class CallActivity extends AppCompatActivity {
 
     EditText edtNumber;
-    Button btnDial;
+    ImageButton btnCall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
-        edtNumber=(EditText)findViewById(R.id.edtNumber);
-        btnDial=(Button)findViewById(R.id.btnDial);
-        btnDial.setOnClickListener(new View.OnClickListener() {
+        edtNumber = findViewById(R.id.edtNumber);
+        btnCall = findViewById(R.id.btnCall);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Auto-generated
-                Intent i=new Intent(Intent.ACTION_CALL);
-                i.setData(Uri.parse("tel: "+ edtNumber.getText().toString()));
-                startActivity(i);
+                String phone = edtNumber.getText().toString();
+                //if phone number is empty
+                if(phone.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter Mobile Number!",Toast.LENGTH_SHORT).show();
+                }
+                //call if number is entered
+                else{
+                    String call = "tel:" + phone;
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse(call));
+                    startActivity(intent);
+                }
             }
         });
     }
