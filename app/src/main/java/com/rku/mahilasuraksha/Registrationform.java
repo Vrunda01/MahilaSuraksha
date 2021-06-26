72ea454ef2a1511c19b9df8fe978d91b12d87d63
Krushalipatoliya <kpatoliya252@rku.ac.in>
2021-06-26 11:06:20 +0530

    Add java code in Registrationform.java page.

--- app/src/main/java/com/rku/mahilasuraksha/Registrationform.java
+++ app/src/main/java/com/rku/mahilasuraksha/Registrationform.java
@@ -2,7 +2,10 @@ package com.rku.mahilasuraksha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registrationform extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class Registrationform extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrationform);
+
+        Button button = (Button) findViewById(R.id.SHOW_PROGRESS);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Phone();
+            }
+
+        });
+
+    }
+
+    public void Phone()
+
+    {
+        Intent intent = new Intent(this, Phone.class);
+        startActivity(intent);
     }
 }
