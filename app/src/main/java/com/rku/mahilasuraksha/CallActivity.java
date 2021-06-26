518135431565e3315401ed34bf9ac567fae24e86
Vrunda01 <vsarvaiya088@rku.ac.in>
2021-06-26 16:51:33 +0530

    Changes in call activity.

--- app/src/main/java/com/rku/mahilasuraksha/CallActivity.java
+++ app/src/main/java/com/rku/mahilasuraksha/CallActivity.java
@@ -1,32 +1,69 @@
 package com.rku.mahilasuraksha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class CallActivity extends AppCompatActivity {
 
-    EditText edtNumber;
-    Button btnDial;
+    private static final int REQUEST_CALL = 1;
+    private EditText edtNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
-        edtNumber=(EditText)findViewById(R.id.edtNumber);
-        btnDial=(Button)findViewById(R.id.btnDial);
-        btnDial.setOnClickListener(new View.OnClickListener() {
+
+        edtNumber = findViewById(R.id.edtNumber);
+        ImageView imgcall = findViewById(R.id.img_call);
+
+        imgcall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Auto-generated
-                Intent i=new Intent(Intent.ACTION_CALL);
-                i.setData(Uri.parse("tel: "+ edtNumber.getText().toString()));
-                startActivity(i);
+                makePhone();
             }
         });
+    }
+
+    private void makePhone() {
+        String number = edtNumber.getText().toString();
+        if(number.trim().length() > 0) {
+            if (ContextCompat.checkSelfPermission(CallActivity.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(CallActivity.this,
+                        new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+            } else {
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+        }
+        else {
+            Toast.makeText(CallActivity.this, "Enter Phone Number!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhone();
+            }
+            else {
+                Toast.makeText(CallActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
