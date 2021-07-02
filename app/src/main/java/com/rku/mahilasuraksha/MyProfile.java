3b7755e3490c394e1573b3e773d178512678abd1
rchauhan471 <rchauhan471@rku.ac.in>
2021-07-02 14:43:44 +0530

    add menu in MyProfile

--- app/src/main/java/com/rku/mahilasuraksha/MyProfile.java
+++ app/src/main/java/com/rku/mahilasuraksha/MyProfile.java
@@ -1,19 +1,25 @@
 package com.rku.mahilasuraksha;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyProfile extends AppCompatActivity {
 
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+    }
 
-        mTextView = (TextView) findViewById(R.id.text);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menukey, menu);
+        return true;
     }
 }
